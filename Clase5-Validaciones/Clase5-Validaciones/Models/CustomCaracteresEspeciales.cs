a194d9397528c7d75e455fd8f61d6b02a2b670b1
SeijasTomas <tomas.seijas10@gmail.com>
2021-05-12 19:54:15 -0300

    se agregaron las validaciones pedidas

---
+++ Clase5-Validaciones/Clase5-Validaciones/Models/CustomCaracteresEspeciales.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Clase5_Validaciones.Models
+{
+    public class CustomCaracteresEspeciales : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            string texto = (string)value;
+            if (String.IsNullOrEmpty(texto))
+            {
+                texto = "";
+            }
+            string regexp = $"^[a-zA-Z0-9.,;]*$";
+
+            if ((Regex.Match(texto,regexp).Success))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- Clase5-Validaciones/Clase5-Validaciones/Models/Mascota.cs
+++ Clase5-Validaciones/Clase5-Validaciones/Models/Mascota.cs
@@ -17,9 +17,17 @@ namespace Clase5_Validaciones.Models
         public int? Peso { get; set; }
         public string Color { get; set; }
         [StringLength(200, ErrorMessage ="No se puede ingresar mas de 200 caracteres")]
+
+        [CustomCaracteresEspeciales(ErrorMessage ="No se puede ingresar caracteres especiales")]
         public string Nombre { get; set; }
+
+        [CustomCaracteresEspeciales(ErrorMessage = "No se puede ingresar caracteres especiales")]
         public string DatosDeContacto { get; set; }
         public List<string> Fotos { get; set; }
+
+        [Required(ErrorMessage ="Campo Requerido")]
+        [EmailAddress(ErrorMessage ="Ingrese un mail valido")]
+        public string EmailDeContacto { get; set; }
 
         public Mascota()
         {
